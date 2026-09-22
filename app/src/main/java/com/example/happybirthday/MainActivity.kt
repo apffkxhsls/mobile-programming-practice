36122c78273a8dc2d95d0206daa3cf423ef1721d
Yeji Kim <135138918+apffkxhsls@users.noreply.github.com>
2026-09-22 17:42:01 +0900

    chore: 주석처리 취소

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
@@ -72,24 +73,24 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     )
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    HappyBirthdayTheme {
-//        GreetingText(
-//            message = "Happy Birthday Sam!",
-//            from = "From Emma"
-//        )
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun BirthdayCardPreview() {
-//    HappyBirthdayTheme {
-//        GreetingImage(
-//            message = "Happy Birthday Sam!",
-//            from = "From Emma"
-//        )
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HappyBirthdayTheme {
+        GreetingText(
+            message = "Happy Birthday Sam!",
+            from = "From Emma"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingImage(
+            message = "Happy Birthday Sam!",
+            from = "From Emma"
+        )
+    }
+}
